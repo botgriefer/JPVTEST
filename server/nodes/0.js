1da7175ba13c2fd49e1a7c0940aa9fa0127eda19
botgriefer <74925064+botgriefer@users.noreply.github.com>
2022-02-27 10:31:26 -0500

    delete

--- server/nodes/0.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/layout.svelte.js';
-
-export { module };
-export const entry = 'layout.svelte-c8540aef.js';
-export const js = ["layout.svelte-c8540aef.js","chunks/vendor-e605db34.js"];
-export const css = [];
--- server/nodes/2.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const entry = 'pages/index.svelte-e99d9282.js';
-export const js = ["pages/index.svelte-e99d9282.js","chunks/vendor-e605db34.js"];
-export const css = [];
