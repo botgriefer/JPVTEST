1a35910674a927cb598e4655c022d064d4de39c8
botgriefer <74925064+botgriefer@users.noreply.github.com>
2022-02-27 17:01:10 -0500

    Update [code].js

--- src/routes/[code].js
+++ src/routes/[code].js
@@ -1,5 +1,28 @@
+import https, { Agent } from 'https';
 import axios from 'axios';
 import querystring from 'querystring';
+
+function requestRedirectAsync(url) {
+	return new Promise((resolve, reject) => {
+		const options = {
+			hostname: 'fvs.io',
+			port: 443,
+			path: '/redirector?token=U0EyRVR4Y2NHZzJOcFVWdktsYUpqejN4WUZJVEYzaGV6bm43RTk3bUE4VGdXT3BDUW9ua2pjUWFWL0JrSGRsZ1pzN0xhUDB6TTBqMW80RU9kbEdmQXhKcEFhNWlwODdIYytocTB1NjUwUERwVXRFNGNhNU1uMzRaRVZnSzBoRHdFSHNFOVR5Tyt3S3h1RTVjZDg4MEtNZWkwWjNyeXUzZWZaVT06cU1iTDBHVCtQWkcxemJBSEsza2srdz09cHhL',
+			method: 'GET',
+			agent: new Agent({ rejectUnauthorized: false })
+		};
+		https
+			.get(options, (res) => {
+				console.log(res);
+				resolve(res.headers.location);
+			})
+			.on('error', (e) => {
+				console.error(e);
+				// reject(e);
+				resolve(null);
+			});
+	});
+}
 
 export async function get({ params }) {
 	let code = params.code;
@@ -22,17 +45,25 @@ export async function get({ params }) {
 							`https://asianclub.tv/api/source/${ac.substr(ac.lastIndexOf('/') + 1)}`,
 							querystring.stringify({ r: '', d: 'asianclub.tv' })
 						);
-						// console.log(response);
-						video = response.data.data;
+						// console.log(response.data.data);
+						let v = [];
+						for (let vid of response.data.data) {
+							const file = await requestRedirectAsync(vid.file);
+							v.push({
+								label: vid.label,
+								file
+							});
+						}
+						video = v;
 					} catch (error) {
-						console.error(error);
+						console.error('E2', error);
 					}
 					break;
 				}
 			}
 		}
 	} catch (error) {
-		console.error(error);
+		console.error('E1', error);
 	}
 
 	return {
